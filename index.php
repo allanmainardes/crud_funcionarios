fe447d8e841e3a595bb3d2bcf4a37ee4af67a508
ditisigataux2 <ditisigataux2@cbm.sc.gov.br>
2022-03-31 21:26:38 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php  
+    /**
+     * Desenvolva um CRUD de funcionários com os seguintes campos (
+     * nome,
+     * data de nascimento,
+     * cpf,
+     * e-mail,
+     * estado civil) 
+     * fazendo as devidas validações.
+     * 
+     * Observações:
+     * - Deve se impedir cpf e e-mail inválidos;
+     * - Desenvolver em PHP;
+     * - Utilizando o banco de dados Mysql;
+     * - Enviar em até 7 dias após o recebimento do e-mail.
+     * Publicar no git ou enviar por e-mail.
+     */
+
+?>
